4e638f9c6bf8ad82a776a700d14065f1539af0b3
eomseona <eomsa98@tukorea.ac.kr>
2025-12-26 19:57:49 -0500

    feat: Store uploaded meal images on AWS S3

--- foodies-starting-project/next.config.js
+++ foodies-starting-project/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "seona-nextjs-demo-users-image.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+// Allow URL in the <Image> component
 
-module.exports = nextConfig
+module.exports = nextConfig;
