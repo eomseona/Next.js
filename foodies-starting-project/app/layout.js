91d7a63b6d46b5dc15b12f4337ec197c480d9c16
eomseona <eomsa98@tukorea.ac.kr>
2025-12-27 01:42:38 -0500

    feat: add metadata(static/dynamic)

--- foodies-starting-project/app/layout.js
+++ foodies-starting-project/app/layout.js
@@ -2,6 +2,11 @@ import MainHeader from "@/components/main-header/main-header";
 import "./globals.css";
 // 루트 레이아웃 -> 전역적으로 적용됨
 
+/**
+ * Global metadata for this layout.
+ * These values serve as the default for all child pages.
+ * If a child page defines its own metadata, it will overwrite or merge with these defaults.
+ */
 export const metadata = {
   title: "NextLevel Food",
   description: "Delicious meals, shared by a food-loving community.",
