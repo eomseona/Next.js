5b74fdc7477ecc469d7dd6a71f1b956115006b70
eomseona <eomsa98@tukorea.ac.kr>
2026-02-06 00:48:25 -0500

    feat: stored data from input content

--- data-mutation/app/new-post/page.js
+++ data-mutation/app/new-post/page.js
@@ -1,9 +1,18 @@
+import { storePost } from "@/lib/posts";
+
 export default function NewPostPage() {
   async function createPost(formData) {
     "use server";
     const title = formData.get("title");
     const image = formData.get("image");
     const content = formData.get("content");
+
+    storePost({
+      imageUrl: "",
+      title,
+      content,
+      userId: 1,
+    });
   }
 
   return (
