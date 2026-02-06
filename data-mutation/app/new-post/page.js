21e09aeee81b575521183169d0955fa7a8f26070
eomseona <eomsa98@tukorea.ac.kr>
2026-02-06 00:42:30 -0500

    feat: generated server action

--- data-mutation/app/new-post/page.js
+++ data-mutation/app/new-post/page.js
@@ -1,8 +1,15 @@
 export default function NewPostPage() {
+  async function createPost(formData) {
+    "use server";
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
+  }
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form>
+      <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
