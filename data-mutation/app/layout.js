b1eeb25e46f638492a2fce854d5946fbd1245c8b
eomseona <eomsa98@tukorea.ac.kr>
2026-02-09 00:09:47 -0500

    feat: set the static metadata

--- data-mutation/app/layout.js
+++ data-mutation/app/layout.js
@@ -1,9 +1,10 @@
-import Header from '@/components/header';
-import './globals.css';
+import Header from "@/components/header";
+import "./globals.css";
 
+// metadata will be overrided if you set the metadata in the page.js
 export const metadata = {
-  title: 'NextPosts',
-  description: 'Browse and share amazing posts.',
+  title: "NextPosts",
+  description: "Browse and share amazing posts.",
 };
 
 export default function RootLayout({ children }) {
