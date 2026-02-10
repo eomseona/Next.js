ef10e6bcb023cf8f8429363182ce7b352f3ffb4c
eomseona <eomsa98@tukorea.ac.kr>
2026-02-10 11:34:13 -0500

    feat: Implement password hashing and user persistence

--- authentication/actions/auth-actions.js
+++ authentication/actions/auth-actions.js
@@ -1,5 +1,8 @@
 "use server";
 
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+
 export async function signup(prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -19,4 +22,7 @@ export async function signup(prevState, formData) {
       errors,
     };
   }
+
+  const hashedPassword = hashUserPassword(password);
+  createUser(email);
 }
